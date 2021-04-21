ced2138fe1f89f4eb71b9139562a493982a49704
wangyq <610300317@qq.com>
2021-04-21 16:34:48 +0800

    springMVC-原生servlet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jdbc
     原生myBatis
+# src
+    注解  annotation
+    反射  reflect
---
+++ springMVC/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1/获取前端参数
+        String method = req.getParameter("method");
+        if(method.equals("add")){
+            req.getSession().setAttribute("msg","add方法执行了");
+        }else{
+            req.getSession().setAttribute("msg","add方法没执行");
+        }
+        //2调用业务层
+
+        //3视图转发或者重定向
+        req.getRequestDispatcher("WEB-INF/jsp/test.jsp").forward(req,resp);//转发
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
