8e8b8fb1338603dbe50fa0ecb6c47d26ef59f653
wangyq <610300317@qq.com>
2021-04-30 17:00:41 +0800

    SSM整合-yaml

--- springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -8,6 +8,12 @@ public class SpringbootApplication {
 
     public static void main(String[] args) {
         //将SpringBoot应用启动
+        /*
+        * 1.判断是否是web项目
+        * 2.查找并加载所有可用初始化器，设置到initializer属性中
+        * 3.找出所有的应用程序监听器，设置到listener监听器中
+        * 4.
+        * */
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
