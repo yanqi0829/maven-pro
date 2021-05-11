694aa01da8d0d53698f1ea1f47229cf58e09e0fd
wangyq <610300317@qq.com>
2021-05-11 13:55:00 +0800

    SpringBoot-引入bootstrap模版（略）

---
+++ springboot/src/main/java/com/example/springboot/config/MyMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 使用自定义类扩展SpringMVC
+ * 详见官网7.1.1. Spring MVC Auto-configuration
+ */
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    //视图跳转
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/nihao").setViewName("test");  http://localhost:8082/nihao 至 test页
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+}
